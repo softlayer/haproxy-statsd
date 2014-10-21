ae163ad2f0b03539d963aefd69e2e6fca534189e
Bruno Bonacci <bruno.bonacci@gmail.com>
2014-10-21 10:28:49 +0100

    Added interesting metrics such as number of 1xx, 2xx, 3xx, 4xx, 5xx requests, and various response time

--- haproxy-statsd.py
+++ haproxy-statsd.py
@@ -54,7 +54,7 @@ def report_to_statsd(stat_rows,
         path = '.'.join([namespace, row['pxname'], row['svname']])
 
         # Report each stat that we want in each row
-        for stat in ['scur', 'smax', 'ereq', 'econ', 'rate', 'bin', 'bout']:
+        for stat in ['scur', 'smax', 'ereq', 'econ', 'rate', 'bin', 'bout', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'qtime', 'ctime', 'rtime', 'ttime']:
             val = row.get(stat) or 0
             udp_sock.sendto(
                 '%s.%s:%s|g' % (path, stat, val), (host, port))
